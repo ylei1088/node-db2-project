9dc21b42c3de4d847ef2311a2867d56e0681dccb
ylei1088 <ylei1088@gmail.com>
2021-10-06 18:51:18 -0500

    mvp

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+  {
+    vin: "4Y1SL65848Z411439",
+    make: "toyota",
+    model: "prius",
+    mileage: 215000,
+    title: "clean",
+    transmission: "manual",
+  },
+  {
+    vin: "JH4DA3350GS005185",
+    make: "toyota",
+    model: "corolla",
+    mileage: 115000,
+    title: "salvage",
+  },
+  {
+    vin: "4V4N99EH3CN554692",
+    make: "ford",
+    model: "fcous",
+    mileage: 1500,
+  },
+];
+
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
